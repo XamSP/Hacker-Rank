d78103b8469e62d2e3b51c3f056c2e799e7b0ae9
XamSP <max0720x@gmail.com>
2018-10-01 19:01:33 -0400

    chunk-array-Udemy from Udemy!

---
+++ chunk-array-Udemy.js
@@ -0,0 +1,10 @@
+function chunky(arr, size) {
+    let fatArrbert = [];
+    for (let i=0; i <= arr.length -1; i += size) {
+      fatArrbert.push(arr.slice(i, i+size));
+      console.log(fatArrbert);
+    }
+    return fatArrbert;
+  }
+  
+  chunky([1,2,3,4,5], 2);
